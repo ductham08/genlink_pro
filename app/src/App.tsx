629f41a58a40e79bb4419f956a2b7bd638cf578b
ductham08 <ductham784@gmail.com>
2025-04-28 18:12:34 +0700

    update

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,6 +5,8 @@ import ProtectedRoute from './components/ProtectedRoute';
 import Dashboard from './components/Dashboard';
 import Statistics from './components/Statistics';
 import Profile from './components/Profile';
+import './App.scss';
+import './styles/base.scss';
 
 function App() {
   return (
